4a7dd8860ae2c17e1d9dee530fcb2b796b97af00
stephenunder <41934385+stephenunder@users.noreply.github.com>
2019-04-04 14:56:04 -0500

    delete unused files and set up 2 useState hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const SNIPPETS = [
+    "Bears, beets, Battlestar Galactica",
+    "What's Forrest Gump's password? 1Forrest1",
+    "Where do programmers like to hangout? The Foo Bar"
+  ];
+  const [snippet, setSnippet] = useState("");
+  const [userText, setUserText] = useState("");
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log("setSnippet", snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
   }
+
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log("current userText", userText);
+  }
+
+  return ( 
+    <div>
+      <h1>TypeRace</h1>
+      <hr />
+      <h2>Snippet</h2>
+      {snippet}
+      <input value={userText} onChange={updateUserText} />
+      <hr />
+      {
+        SNIPPETS.map((SNIPPET, index) => (
+          <button onClick={chooseSnippet(index)} key={index}>
+            {SNIPPET.substring(0, 10)}...
+          </button>
+        ))
+      }
+    </div>
+  )
 }
 
 export default App;
